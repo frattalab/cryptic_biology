1b8dfe75aaac6ea675d5ece6b5dfbc91edd20795
Anna-Leigh Brown <brown.annaleigh@gmail.com>
2025-06-17 14:31:58 +0100

    Update trial_nmd_plots.R
    additional flexibility to this function

--- scripts/trial_nmd_plots.R
+++ scripts/trial_nmd_plots.R
@@ -1,5 +1,11 @@
-slope_plot_nmd <- function(big_data, gene_list){
+slope_plot_nmd <- function(big_data, gene_list, input_list = c("Control_Control",
+                                                                             "Cycloheximide_Control", 
+                                                                             "Control_TDP43KD", 
+                                                                             "Cycloheximide_TDP43KD"),
+                       nmd_label = "CHX treatment"){
+
 big_data_filtereds <- big_data %>%
+    filter(.id %in% input_list) %>% 
   group_by(lsv_junc) %>%
   dplyr::filter(n() == 4) %>% #???
   dplyr::filter(de_novo_junctions == 1) %>%
@@ -11,36 +17,45 @@ big_data_filtereds <- big_data %>%
            mean_psi_per_lsv_junction[.id == input_list[4]] - mean_psi_per_lsv_junction[.id == input_list[1]] > 0.05)) %>%
   #dplyr::filter(mean_psi_per_lsv_junction[.id == input_list[4]] - mean_psi_per_lsv_junction[.id == input_list[3]] > 0) %>%# &
   mutate(color_gene_name = as.character(ifelse(mean_psi_per_lsv_junction[.id == input_list[4]] - mean_psi_per_lsv_junction[.id == input_list[3]] > 0.1, 
-                                               "NMD rescued", "non-NMD rescued"))) %>%
-  mutate(alpha_gene_name = as.character(ifelse(mean_psi_per_lsv_junction[.id == input_list[4]] > 0.25 &
-                                                 gene_name %in% gene_list &
-                                                 paste_into_igv_junction %in% c("chr19:17641556-17642414",
-                                                                                "chr6:152247944-152249161",
-                                                                                "chr8:79611214-79616822",
-                                                                                "chr19:4492152-4493703",
-                                                                                "chr5:157361336-157361468",
-                                                                                "chr16:70271972-70272796"), 1, 0))) %>%
-  
-  
-  #"chr19:17641556-17642414","chr8:79611214-79616822"), #UNC13A, STMN2
-                                                #                                "chr19:7169831-7170537", "chr19:7168102-7169727", "chr16:70271972-70272796") #AARS1
-                                                                                #"chr5:157361336-157361468", #CYFIP2
-                                                                                #"chr6:152247944-152249161" #, "chr6:152244656-152247823" #SYNE1
-                                                                              
-                                                                                #"chr20:63439708-63444659","chr10:3099819-3101365",
-                                                                                #"chr2:241668985-241670726","chr19:7169831-7170537"
-                                                #                                1, 0))) %>% 
-  mutate(label_junction = case_when(.id == input_list[4] & mean_psi_per_lsv_junction[.id == input_list[4]] > 0.2 &
-                                      alpha_gene_name == 1 ~ gene_name, T ~ ""))
+                                               "NMD rescued", "non-NMD rescued")))
+  # mutate(alpha_gene_name = as.character(ifelse(mean_psi_per_lsv_junction[.id == input_list[4]] > 0.25 &
+  #                                                gene_name %in% gene_list,
+  #                                                # paste_into_igv_junction %in% c("chr19:17641556-17642414",
+  #                                                #                                "chr6:152247944-152249161",
+  #                                                #                                "chr8:79611214-79616822",
+  #                                                #                                "chr19:4492152-4493703",
+  #                                                #                                "chr5:157361336-157361468",
+  #                                                #                                "chr16:70271972-70272796"), 
+  #                                              1, 0))) %>%
+  # 
+  # mutate(label_junction = case_when(.id == input_list[4] & mean_psi_per_lsv_junction[.id == input_list[4]] > 0.2 &
+  #                                     alpha_gene_name == 1 ~ gene_name, T ~ NA_character_))
                                            #gene_name %in% gene_list ~ gene_name, T ~ ""))
-table(big_data_filtereds$color_gene_name)
+highlighting_table = big_data_filtereds %>% 
+    filter(gene_name %in% gene_list) %>% 
+    filter(.id == c(input_list[4])) %>% 
+    group_by(gene_name) %>% 
+    slice_max(mean_psi_per_lsv_junction) %>% 
+    mutate(alpha_gene_name = 1) %>% 
+    mutate(label_junction = gene_name) %>% 
+    distinct(paste_into_igv_junction,gene_name,alpha_gene_name,label_junction)
+
+    
 
 #big_data_filtereds_list <- big_data_filtereds %>% filter(color_gene_name == "Delta PSI > 0.05") %>% pull(gene_name) %>% unique()
 
 #rite.table(big_data_filtereds, "~/Desktop/nmd_or_not.csv", quote = F, row.names = F, sep = ",")
 
-ploss <- big_data_filtereds %>%
+ploss = big_data_filtereds %>%
   dplyr::filter(.id %in% c(input_list[3],input_list[4])) %>%
+     left_join(highlighting_table) %>% 
+     mutate(label_junction = ifelse(.id == input_list[3], NA_character_,label_junction)) %>% 
+     mutate(alpha_gene_name = ifelse(is.na(alpha_gene_name),0.4,alpha_gene_name)) %>% 
+    group_by(paste_into_igv_junction,.id) %>% 
+    mutate(label_junction = ifelse(mean_psi_per_lsv_junction == max(mean_psi_per_lsv_junction),label_junction, NA_character_)) %>% 
+    mutate(alpha_gene_name = ifelse(mean_psi_per_lsv_junction == max(mean_psi_per_lsv_junction),alpha_gene_name,0.4)) %>% 
+    ungroup() %>% 
+
   ggplot(mapping = aes(x = .id, y = mean_psi_per_lsv_junction)) +
   facet_wrap(facets = vars(color_gene_name)) +
   geom_point(aes(color = color_gene_name, alpha = alpha_gene_name, group = lsv_junc), show.legend = F) + 
@@ -53,16 +68,19 @@ ploss <- big_data_filtereds %>%
   scale_color_manual(values = c(#"#ABDDA4", 
                                 "#3288BD",
                                 "#D53E4F")) +
-  scale_alpha_manual(values = c(0.02,1)) +
+  # scale_alpha_manual(values = c(0.2,1)) +
   xlab("") +
   ylab("PSI") +
-  scale_x_discrete(labels = c("TDP43-KD", "TDP43-KD + \n NMD inhibition")) +
+  scale_x_discrete(labels = c("TDP43-KD", paste0("TDP43-KD\n+\n",nmd_label))) +
   scale_y_continuous(limits = c(0,1), breaks = c(0,0.25,0.5,0.75,1), labels = c("0%", "25%", "50%", "75%", "100%")) +
   theme_classic() +
   theme(text = element_text(size = 18))
 plot(ploss)
+
+return_list = list(ploss,big_data_filtereds)
+names(return_list) = c('plot','filtered_table')
 #ggsave(filename = "~/Desktop/upf1_slope.png", ploss)
-return(ploss)
+return(return_list)
 }
 
 #avg_read_counts <- featureCounts %>%
